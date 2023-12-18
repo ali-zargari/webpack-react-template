bc30df3214d8641ca080d529784fa6c9a5837916
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2023-12-17 20:37:19 -0800

    Can now serve dist folder to the server ot test final product.

---
+++ src/components/Info.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+//import "../../styles/App.css";
+//import App from "./App/App.jsx";
+
+
+function info_panel(){
+	const [num1, setNum] = useState(0);
+	setNum(num1+1);
+
+
+	return (
+		<>
+
+		</>
+	);
+
+}
+
+export default info_panel;
